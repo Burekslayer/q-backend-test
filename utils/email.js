2b8b4750dc98ab77aa675fa09ada492bc4cbdce3
Burekslayer <ivanscvetkovic@gmail.com>
2025-04-04 23:26:36 +0200

    changed email.js

--- utils/email.js
+++ utils/email.js
@@ -5,34 +5,47 @@ let transporter;
 
 async function setupTransporter() {
   if (!transporter) {
-    const account = await etherealTestAccount();
-    transporter = nodemailer.createTransport({
-      host: account.smtp.host,
-      port: account.smtp.port,
-      secure: account.smtp.secure,
-      auth: {
-        user: account.user,
-        pass: account.pass,
-      },
-    });
+    if (process.env.NODE_ENV === 'production') {
+      transporter = nodemailer.createTransport({
+        host: process.env.EMAIL_HOST, // smtp.gmail.com
+        port: process.env.EMAIL_PORT, // 465
+        secure: true,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+    } else {
+      const account = await etherealTestAccount();
+      transporter = nodemailer.createTransport({
+        host: account.smtp.host,
+        port: account.smtp.port,
+        secure: account.smtp.secure,
+        auth: {
+          user: account.user,
+          pass: account.pass,
+        },
+      });
+    }
   }
   return transporter;
 }
 
-
 async function sendVerificationEmail(to, token) {
-  console.log('📤 Sending email to:', to); // <--- ADD THIS LINE
+  console.log('📤 Sending email to:', to);
 
   const transporter = await setupTransporter();
+  const verificationLink = `${process.env.FRONTEND_URL || 'http://localhost:3000'}/verify-email?token=${token}`;
+
   const info = await transporter.sendMail({
     from: '"Kreativni Univerzum" <no-reply@kreativni.univerzum.com>',
     to,
     subject: 'Verify your account',
     html: `<p>Please verify your email by clicking the link below:</p>
-           <a href="http://localhost:5000/api/users/verify?token=${token}">Verify Email</a>`
+           <a href="${verificationLink}">Verify Email</a>`
   });
 
+  console.log('📬 Email sent:', info.messageId);
 }
-
 
 module.exports = { sendVerificationEmail };
